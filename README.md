3832e372816f7ee1cdb7b14bcee5df828b650660
Alexandergmzx <alexandergmzx@gmail.com>
2020-12-27 16:09:23 -0600

    [reqs] The guessing game was updated to fulfill the 1 fuction requirement

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Project: The file number guessing game
 
 **DATE:**
-dimanche 27 décembre 2020, 15:46:20 (UTC-0600)
+dimanche 27 décembre 2020, 16:08:37 (UTC-0600)
  
 **NUMBER OF LINES:**
-21
+24
--- guessinggame.sh
+++ guessinggame.sh
@@ -5,17 +5,20 @@ archivos=($(ls))
 
 num_archivos=${#archivos[*]}
 
-echo "Guess how many files are in the current directory? Type a number and then press Enter"
+function ask_user {
+	echo "Guess how many files are in the current directory? Type a number and then press Enter"
+	read guess
+}
 
-read guess
+ask_user
 
 while [[ $guess != $num_archivos ]]; do
 	if [[ guess -lt num_archivos ]]; then
-		echo "The guess number is lower than the real number, try again:"
+		echo "The guess number is lower than the real number"
 	else
-		echo "The guess number is higher than the real number, try again:"
+		echo "The guess number is higher than the real number"
 	fi
-	read guess
+	ask_user
 done
 
 echo "Yes! The real number is" $num_archivos "and the one given is $guess. Congratulations!"
